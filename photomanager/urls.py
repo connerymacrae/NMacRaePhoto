a2f5d7cef1682d64bcad9b35eae31019ec5ee269
Connery MacRae <macrae.connery@gmail.com>
2024-04-17 16:48:18 -0400

    Update urls.py
    created path for author detail

--- photomanager/urls.py
+++ photomanager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.index, name='index'),
-
+    path('', views.index, name='index'),
+    path('photographer/<int:pk>', views.PhotographerDetailView.as_view(),
+         name='photographer-detail'),
 ]
