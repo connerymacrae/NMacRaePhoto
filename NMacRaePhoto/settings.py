1c6ed515c41127f8ea3c6e454573f5418f30be81
Connery MacRae <macrae.connery@gmail.com>
2024-05-08 15:42:54 -0400

    added app to settings and urls

--- NMacRaePhoto/settings.py
+++ NMacRaePhoto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photomanager.apps.PhotomanagerConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
