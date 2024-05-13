bb83b30cb60002b44b62b12dfb9079c84461bf62
Connery MacRae <macrae.connery@gmail.com>
2024-05-13 16:00:17 -0400

    messing with tests

--- accounts/tests/test_signup_and_accounts.py
+++ accounts/tests/test_signup_and_accounts.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from accounts.forms import UserRegisterForm
+from django.urls import reverse, resolve
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -24,17 +25,8 @@ class RegisterUserTests(TestCase):
         self.assertEqual(response.status_code, 302)
         created_user = User.objects.get(username='test')
         self.assertEqual(created_user.email, 'john@john.com')
-        #self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertRedirects(response, "/photomanager/")
+        #self.assertTemplateUsed(follow_on_response, 'photomanager:index')
 
 
-class AccountsTest(TestCase):
-    def setUp(self):
-        test_user = User.objects.create_user({'username': 'johnjohn',
-                                              'email': 'john@john.com',
-                                              'password1': 'XD78xd87!',
-                                              'password2': 'XD78xd87!'})
-        test_user.save()
-
-    def test_user_can_login(self):
-        login = self.client.login(username='johnjohn', password='XD78xd87!)
-        response = self.client.get('/accounts')
+# class AccountsTest(TestCase):
